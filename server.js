54dbb24d45cdfa7f3af8f7d34897fa205a65615e
Girijajethitech <girijashankarmohanta11@gmail.com>
2023-11-19 10:42:18 +0530

    jhbjbb

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
+const path = require('path');
 const app = express();
 const port = process.env.port || 5000;
 
